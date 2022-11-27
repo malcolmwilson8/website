840016ec2f6086cd04cc9cde91b82a577f40a046
Malcolm Wilson <117777716+malcolmwilson8@users.noreply.github.com>
2022-11-27 21:17:10 +0000

    Updated scripts, removing unnecessary append

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ button1.addEventListener('click', spinContentLeft);
 button2.addEventListener('click', spinContentRight);
 infoButton.addEventListener('click', spinnerInfo);
 
-//Define function for left button
+//Define function for left button with DOM
 function spinContentLeft(){
     spinner = document.querySelector('.spinner');
     angle = angle - 60;
@@ -18,34 +18,34 @@ function spinContentLeft(){
     spinner.setAttribute("style","transform: rotateY("+ angle +"deg);");
 }
 
-//Define function for right button
+//Define function for right button with DOM
 function spinContentRight(){
     spinner = document.querySelector('.spinner');
-        angle = angle + 60;
+    angle = angle + 60;
 
     spinner.setAttribute("style","transform: rotateY("+ angle +"deg);");
 }
 
 //Define function to show text after click on spinner button, and be clickable only once
 function spinnerInfo(){
-    let descriptionText = document.createElement('div')
-    let newText = document.createTextNode("This is a 3d gallery displaying my completed projects for my Founders and Coders application. " + 
+        let descriptionText = document.createElement('div')
+        let newText = document.createTextNode("This is a 3d gallery displaying my completed projects for my Founders and Coders application. " + 
 
-    "My first project was focused on building a page dedicated to my hobby of skateboarding. " +
+        "My first project was focused on building a page dedicated to my hobby of skateboarding. " +
+     
+        "My second project entailed building a gallery displaying the projects I`ve been working on. " +
+
+        "My third project centered around building a fully functional comment box system. " +
+
+        "For my fourth project I built a user interface using HTML, and populated it with data from a JavaScript object containing data from some of Wes Anderson's films. " +
  
-    "My second project entailed building a gallery displaying the projects I`ve been working on. " +
+        "For project five I built this website to support my application. " +
 
-    "My third project centered around building a fully functional comment box system. " +
-
-    "For my fourth project I built a user interface using HTML, and populated it with data from a JavaScript object containing data from some of Wes Anderson's films. " +
-
-    "For project five I built this website to support my application. " +
-
-    "My sixth and final project was to build a feature to host on my website; the 3d gallery you see above. " +
-    
-    "Links to the deployed project pages are below!");
-    descriptionText.appendChild(newText);
-    let text = document.getElementsByClassName('info-holder')[0];
-    text.appendChild(newText);
-    infoButton.removeEventListener('click', spinnerInfo);
+        "My sixth and final project was to build a feature to host on my website; the 3d gallery you see above. " +
+        
+        "Links to the deployed project pages are below!");
+        let text = document.getElementsByClassName('info-holder')[0];
+        text.appendChild(newText);
+        infoButton.removeEventListener('click', spinnerInfo);
 }
+
